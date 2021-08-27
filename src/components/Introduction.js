8b510170586eb8fab1824a670809c3ba3b2e67a9
abhi00 <abhijit.pandit808@gmail.com>
2021-08-27 18:55:54 +0100

    added animation idea, glowing moon, stars, reflection plane

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,20 +1,19 @@
 import React from "react";
 import * as THREE from "three";
-import { setupScene, introAnimation } from "./visuals/graphics.js"
+import { setupScene, Animation1 } from "./visuals/graphics.js"
 
 class Introduction extends React.Component{
     componentDidMount() {
-        const [scene, camera, renderer] = setupScene(new THREE.Vector3(0,0,2))
+        const [scene, camera, renderer] = setupScene(new THREE.Vector3(0,0,100))
         this.mount.appendChild(renderer.domElement);
 
-        introAnimation(scene, camera, renderer)
+        Animation1(scene, camera, renderer)
     }
 
     render(){
         return(
             <div className="introduction">
                 <div className="introduction-animation" ref={ref => (this.mount = ref)}/>
-                <div className="introduction-text">Coming Soon</div>
             </div>  
         )
     }
--- src/components/visuals/graphics.js
+++ src/components/visuals/graphics.js
@@ -1,5 +1,6 @@
 import * as THREE from "three";
-import sphere_texture from "../../assets/animations/intro/mercury.jpg"
+import {Water} from 'three/examples/jsm/objects/Water.js';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 
 /*ORIGINALLY FROM EOVISUALISER*/
 
@@ -19,9 +20,13 @@ function setupScene(starting_camera_pos){
     //setup renderer
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.setPixelRatio(window.devicePixelRatio);
 
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+    const controls = new OrbitControls(camera, renderer.domElement);
+    controls.update();
 
     return [scene, camera, renderer]
 }
@@ -39,27 +44,79 @@ function loadTexture(image_object){
 }
 
 /**
- * Animation 1: The animation for the introduction component
+ * Animation 1: Moon and Ocean Animation
  * @param scene the scene into which to render
  * @param camera the camera for the animation
  * @param renderer the renderer
  */
-function introAnimation(scene, camera, renderer){
-    const sun_light = new THREE.PointLight(0xffffff, 1, 0, 2);
-    sun_light.position.set(0,5,0);
-    sun_light.castShadow = true;
-        
-    const sphere_geometry = new THREE.SphereBufferGeometry(1, 80, 80);
-    const sphere_material = new THREE.MeshStandardMaterial({
-        metalness: 0.3,
-        roughness: 0.8,
-        map: loadTexture(sphere_texture)
-    })
-    const sphere = new THREE.Mesh(sphere_geometry, sphere_material);
-    sphere.position.set(0,0,0)
+function Animation1(scene, camera, renderer){     
+    /*
+    Plan:
+    1. Big Glowing Moon
+    2. Dark Blue Sky
+    3. Small Glowing Stars
+    4. Reflective Ocean Plane
+    5. Mountains
+    6. Train moving across train track
+    7? Clouds moving left to right/small birds in sky/rocket launching/shooting star
+    */
 
-    scene.add(sun_light);
-    scene.add(sphere);
+    //create reusable geometries
+    const sphere_geometry = new THREE.SphereBufferGeometry(30, 50, 50);
+    const small_sphere_geometry = new THREE.SphereBufferGeometry(1, 4, 4);
+    const plane_geometry = new THREE.PlaneBufferGeometry(3000, 3000, 300, 300);
+    const box_geometry = new THREE.BoxBufferGeometry(5,3,3);
+
+    //create sky
+    const sky_material = new THREE.MeshBasicMaterial({color: 0x00000d})
+    const sky = new THREE.Mesh(plane_geometry, sky_material);
+    sky.position.set(0,0,-500);
+    scene.add(sky);
+
+    //create stars
+    const star_material = new THREE.MeshBasicMaterial({color: 0xffffff});
+    const stars = [];
+    for (let i = 0; i < 500; i++) {
+        const star = new THREE.Mesh(small_sphere_geometry, star_material);
+        const x = THREE.MathUtils.randFloatSpread(1000);
+        const y = THREE.MathUtils.randFloatSpread(1000);
+        const z = THREE.MathUtils.randFloatSpread(250) - 250;
+        star.position.set(x,y,z);
+        stars.push(star);
+        scene.add(star);
+    }
+    
+    //create moon
+    const moon_material = new THREE.MeshBasicMaterial({
+        color: 0xffffff,
+        transparent: true,});
+    const moon = new THREE.Mesh(sphere_geometry, moon_material);
+    moon.position.set(40,5,-30);
+    scene.add(moon);
+
+    //create moon light
+    const moon_light = new THREE.PointLight(0xffffff,1,0,2);
+    moon_light.position.set(80,80,-30);
+    scene.add(moon_light);
+
+    //create train
+    const train_material = new THREE.MeshBasicMaterial({color:0x800000});
+    const train = new THREE.Mesh(box_geometry, train_material);
+    train.position.set(-100,0,-10)
+    scene.add(train);
+
+    //create ocean
+    const water = new Water(
+        plane_geometry,
+        {
+            sunColor: 0xffffff,
+            waterColor: 0x00000d,
+            distortionScale: 3.7
+        }
+    );
+    water.rotation.x = - Math.PI / 2;
+    water.position.set(-30,-30,0);
+    scene.add( water );
 
     /**
      * animator
@@ -67,8 +124,9 @@ function introAnimation(scene, camera, renderer){
     function animate(){
         requestAnimationFrame(animate);
 
-        sphere.rotation.y += 0.005;
-
+        const time = performance.now() * 0.001;
+        moon.position.y = Math.sin(time) * 2 + 40;
+        train.position.x += 0.5;
         renderer.render(scene, camera);
     };
     animate();    
@@ -78,5 +136,5 @@ function introAnimation(scene, camera, renderer){
 export {
     setupScene,
     loadTexture,
-    introAnimation
+    Animation1
 }
