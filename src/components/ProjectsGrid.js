696da7627e174ec9d7dcd213f34b3a82c9f98867
abhi00 <abhijit.pandit808@gmail.com>
2021-10-14 12:53:08 +0100

    added close functionality to project description

--- src/components/ProjectsGrid.js
+++ src/components/ProjectsGrid.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ProjectTile } from './components.js';
-import { OpenInNew } from '@material-ui/icons';
+import { OpenInNew, Close } from '@material-ui/icons';
 import githubIcon from '../assets/icons/github.png';
 
 class ProjectsGrid extends React.Component {
@@ -24,7 +24,7 @@ class ProjectsGrid extends React.Component {
         var projectTiles = [];
         for (var i = 0; i < this.props.projectsJSON.length; i++){
             var projectJSON = this.props.projectsJSON[i];
-            projectTiles.push(<ProjectTile projectID={i} imageUrl={projectJSON.pictures.tile} renderDescription={this.renderDescription}/>);
+            projectTiles.push(<ProjectTile projectID={i} imageUrl={projectJSON.pictures.tile} renderDescription={this.toggleDescription}/>);
             if ((i+1) % this.consts.projectsPerLine === 0 || i === this.props.projectsJSON.length - 1){
                 projectRows.push(<div className='projectsgrid-row-container'>{projectTiles}</div>);
                 projectTiles = [];
@@ -33,15 +33,13 @@ class ProjectsGrid extends React.Component {
         this.setState({projectRows: projectRows});
     }
 
-    renderDescription = (e) => {
+    toggleDescription = (e) => {
         this.setState({
-            descriptionShown: true,
+            descriptionShown: !this.state.descriptionShown,
             descriptionProjectID: JSON.parse(e.currentTarget.getAttribute('projectID')),
             descriptionProject: this.props.projectsJSON[JSON.parse(e.currentTarget.getAttribute('projectID'))]
         });
     }
-
-    /* TODO Add cross */
 
     render(){
         return(
@@ -52,7 +50,7 @@ class ProjectsGrid extends React.Component {
                     <div className='projectsgrid-description-container'>
                         <div className='projectsgrid-description-content-container'>
                             <div className='projectsgrid-description-leftcol-container'>
-                            <div className='projectsgrid-description-title'>{this.state.descriptionProject.name}</div>
+                                <div className='projectsgrid-description-title'>{this.state.descriptionProject.name}</div>
                                 <div className='projectsgrid-description-text'>{this.state.descriptionProject.description}</div>
                                 <div className='projectsgrid-description-tech'>{this.state.descriptionProject.techs}</div>
                                 <div className='projectsgrid-description-links-container'>
@@ -63,10 +61,10 @@ class ProjectsGrid extends React.Component {
                                         <img src={githubIcon} className='projectsgrid-description-icon-github' alt=''/>
                                     </a>
                                 </div>
+                                <Close onClick={this.toggleDescription} className='projectsgrid-description-icon-close'/>
                             </div>
                             <img src={require('../assets/projects/'+this.state.descriptionProject.pictures.tile).default} alt='' className='projectsgrid-description-image'/>
                         </div>
-                        
                     </div>
                 }
             </div>
